d92e15393233b8b18148bc54bebdf7d35767c83b
luis <lfflorian4@gmail.com>
2019-03-26 21:22:50 -0600

    readme

--- LoginDesing/Form1.Designer.cs
+++ LoginDesing/Form1.Designer.cs
@@ -41,14 +41,15 @@
             // 
             this.materialSingleLineTextField1.Depth = 0;
             this.materialSingleLineTextField1.Hint = "Username";
-            this.materialSingleLineTextField1.Location = new System.Drawing.Point(12, 83);
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(16, 102);
+            this.materialSingleLineTextField1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
             this.materialSingleLineTextField1.PasswordChar = '\0';
             this.materialSingleLineTextField1.SelectedText = "";
             this.materialSingleLineTextField1.SelectionLength = 0;
             this.materialSingleLineTextField1.SelectionStart = 0;
-            this.materialSingleLineTextField1.Size = new System.Drawing.Size(276, 23);
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(368, 28);
             this.materialSingleLineTextField1.TabIndex = 0;
             this.materialSingleLineTextField1.UseSystemPasswordChar = false;
             // 
@@ -56,14 +57,15 @@
             // 
             this.materialSingleLineTextField2.Depth = 0;
             this.materialSingleLineTextField2.Hint = "Password";
-            this.materialSingleLineTextField2.Location = new System.Drawing.Point(12, 125);
+            this.materialSingleLineTextField2.Location = new System.Drawing.Point(16, 154);
+            this.materialSingleLineTextField2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.materialSingleLineTextField2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialSingleLineTextField2.Name = "materialSingleLineTextField2";
             this.materialSingleLineTextField2.PasswordChar = '*';
             this.materialSingleLineTextField2.SelectedText = "";
             this.materialSingleLineTextField2.SelectionLength = 0;
             this.materialSingleLineTextField2.SelectionStart = 0;
-            this.materialSingleLineTextField2.Size = new System.Drawing.Size(276, 23);
+            this.materialSingleLineTextField2.Size = new System.Drawing.Size(368, 28);
             this.materialSingleLineTextField2.TabIndex = 1;
             this.materialSingleLineTextField2.UseSystemPasswordChar = false;
             // 
@@ -72,13 +74,13 @@
             this.materialCheckBox1.AutoSize = true;
             this.materialCheckBox1.Depth = 0;
             this.materialCheckBox1.Font = new System.Drawing.Font("Roboto", 10F);
-            this.materialCheckBox1.Location = new System.Drawing.Point(13, 169);
+            this.materialCheckBox1.Location = new System.Drawing.Point(17, 208);
             this.materialCheckBox1.Margin = new System.Windows.Forms.Padding(0);
             this.materialCheckBox1.MouseLocation = new System.Drawing.Point(-1, -1);
             this.materialCheckBox1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialCheckBox1.Name = "materialCheckBox1";
             this.materialCheckBox1.Ripple = true;
-            this.materialCheckBox1.Size = new System.Drawing.Size(120, 30);
+            this.materialCheckBox1.Size = new System.Drawing.Size(143, 30);
             this.materialCheckBox1.TabIndex = 2;
             this.materialCheckBox1.Text = "Remember me";
             this.materialCheckBox1.UseVisualStyleBackColor = true;
@@ -86,11 +88,12 @@
             // materialRaisedButton1
             // 
             this.materialRaisedButton1.Depth = 0;
-            this.materialRaisedButton1.Location = new System.Drawing.Point(213, 228);
+            this.materialRaisedButton1.Location = new System.Drawing.Point(284, 281);
+            this.materialRaisedButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialRaisedButton1.Name = "materialRaisedButton1";
             this.materialRaisedButton1.Primary = true;
-            this.materialRaisedButton1.Size = new System.Drawing.Size(75, 23);
+            this.materialRaisedButton1.Size = new System.Drawing.Size(100, 28);
             this.materialRaisedButton1.TabIndex = 4;
             this.materialRaisedButton1.Text = "LOGIN";
             this.materialRaisedButton1.UseVisualStyleBackColor = true;
@@ -99,10 +102,11 @@
             // 
             this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.materialDivider1.Depth = 0;
-            this.materialDivider1.Location = new System.Drawing.Point(12, 257);
+            this.materialDivider1.Location = new System.Drawing.Point(16, 316);
+            this.materialDivider1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialDivider1.Name = "materialDivider1";
-            this.materialDivider1.Size = new System.Drawing.Size(276, 1);
+            this.materialDivider1.Size = new System.Drawing.Size(368, 1);
             this.materialDivider1.TabIndex = 5;
             // 
             // materialLabel1
@@ -111,11 +115,12 @@
             this.materialLabel1.BackColor = System.Drawing.Color.Transparent;
             this.materialLabel1.Depth = 0;
             this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
-            this.materialLabel1.ForeColor = System.Drawing.Color.Gray;
-            this.materialLabel1.Location = new System.Drawing.Point(13, 277);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(17, 341);
+            this.materialLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel1.Name = "materialLabel1";
-            this.materialLabel1.Size = new System.Drawing.Size(152, 19);
+            this.materialLabel1.Size = new System.Drawing.Size(190, 24);
             this.materialLabel1.TabIndex = 6;
             this.materialLabel1.Text = "Lost your password ?";
             // 
@@ -125,19 +130,20 @@
             this.materialLabel2.BackColor = System.Drawing.Color.Transparent;
             this.materialLabel2.Depth = 0;
             this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
-            this.materialLabel2.ForeColor = System.Drawing.Color.Silver;
-            this.materialLabel2.Location = new System.Drawing.Point(224, 348);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(299, 428);
+            this.materialLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
-            this.materialLabel2.Size = new System.Drawing.Size(64, 19);
+            this.materialLabel2.Size = new System.Drawing.Size(80, 24);
             this.materialLabel2.TabIndex = 7;
             this.materialLabel2.Text = "Register";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 376);
+            this.ClientSize = new System.Drawing.Size(400, 463);
             this.Controls.Add(this.materialLabel2);
             this.Controls.Add(this.materialLabel1);
             this.Controls.Add(this.materialDivider1);
@@ -145,7 +151,9 @@
             this.Controls.Add(this.materialCheckBox1);
             this.Controls.Add(this.materialSingleLineTextField2);
             this.Controls.Add(this.materialSingleLineTextField1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
+            this.Sizable = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
---
+++ README.md
@@ -0,0 +1,18 @@
+# Material Design Login
+
+![Alt text](https://i.ibb.co/M542GZP/Material-Design.png?raw=true "Interfaz")
+
+## Descripción
+
+Interfaz de logín desarrollado con **Material Design** para una aplicación de escritorio windows.
+
+## Herramientas
+
+- C# : Lenguaje de programación
+- Visual Studio 2017 : IDE 
+- Material Design : Modulo de NuGet para la personalización de interfaces
+
+## Referencias
+
+https://www.youtube.com/watch?v=jDCquDMNNJw
+
